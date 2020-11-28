7ad3f05fa6f12c92808594973630197f5f18018a
dlondres420 <62063056+dlondres420@users.noreply.github.com>
2020-11-27 23:29:18 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -3,10 +3,18 @@ import pygame
 pygame.init()
 
 walkRight = [pygame.image.load('R1.png'), pygame.image.load('R2.png'), pygame.image.load('R3.png'),
-             pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png')]
+             pygame.image.load('R4.png'), pygame.image.load('R5.png'), pygame.image.load('R6.png'),
+             pygame.image.load('R7.png'), pygame.image.load('R8.png'),
+             pygame.image.load('R9.png'), pygame.image.load('R10.png'), pygame.image.load('R11.png'),
+             pygame.image.load('R12.png')]
 walkLeft = [pygame.image.load('L1.png'), pygame.image.load('L2.png'), pygame.image.load('L3.png'),
-            pygame.image.load('L4.png'), pygame.image.load('L5.png'), pygame.image.load('L6.png')]
+            pygame.image.load('L4.png'), pygame.image.load('L5.png'), pygame.image.load('L6.png'),
+            pygame.image.load('L7.png'), pygame.image.load('L8.png'), pygame.image.load('L9.png'),
+            pygame.image.load('L10.png'), pygame.image.load('L11.png'), pygame.image.load('L12.png')]
 
+jump_s = pygame.image.load('standingJUMP.png')
+jump_l = pygame.image.load('LJUMP.png')
+jump_r = pygame.image.load('RJUMP.png')
 char = pygame.image.load('standing.png')
 
 window = pygame.display.set_mode((1100, 450))
@@ -21,16 +29,25 @@ def redrawGameWindow():
     global walkCount
 
     window.blit(background, (0, 0))
-    if walkCount + 1 >= 19:
+    if walkCount + 1 >= 37:
         walkCount = 0
 
-    if left:
+    if isJump and not left and not right:
+        window.blit(jump_s, (x,y))
+
+    if isJump and left:
+        window.blit(jump_l, (x, y))
+
+    if isJump and right:
+        window.blit(jump_r, (x,y))
+
+    if left and not isJump:
         window.blit(walkLeft[walkCount // 3], (x, y))
         walkCount += 1
-    elif right:
+    elif right and not isJump:
         window.blit(walkRight[walkCount // 3], (x, y))
         walkCount += 1
-    else:
+    if not left and not right and not isJump:
         window.blit(char, (x, y))
         walkCount = 0
 
@@ -82,7 +99,7 @@ while intro:
     clock.tick(15)
 
 while run:
-    clock.tick(30)
+    clock.tick(50)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
